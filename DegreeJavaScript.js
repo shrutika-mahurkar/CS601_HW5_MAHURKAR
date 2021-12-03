e7536b73d04852b3125891863fc1bfb3246c02dd
shrutika-mahurkar <shrutika.vmahurkar@gmail.com>
2021-12-02 19:47:47 -0800

    error message added

--- DegreeJavaScript.js
+++ DegreeJavaScript.js
@@ -1,12 +1,15 @@
-async function jsonFetch(url){
-   
+async function jsonFetch(url="https://shrutika-mahurkar.github.io/CS601_HW5_MAHURKAR/Degree.json"){
+  
     let response = await fetch(url);
+    
     if(response.ok){
         let json = await response.json();
+        console.log(json);
     }else{
-        alert("HTTP Error : " + response.status);
+        document.getElementById("error").innerHTML ="HTTP Error : " + response.status; 
+
+
+         
     }
 
 }
-
-jsonFetch("./Degree.json");
